fe256e6b2578a7277d1032963dc2a150c27f5e6b
stevenjmarsh <sjm.atl@gmail.com>
2016-12-19 18:03:14 -0500

    correcting case in name of routes export

--- imports/config/routes.jsx
+++ imports/config/routes.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import { Router, Route, IndexRoute, browserHistory } from "react-router";
 import AppContainer from "../ui/containers/AppContainer";
 
-const Routes = (
+const routes = (
   <Router history={browserHistory}>
     <Route path="/" component={AppContainer} />
   </Router>
 );
 
-export default Routes;
+export default routes;
