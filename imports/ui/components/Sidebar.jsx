14df3c9aca966bc4b22d9bf07b27fc9c4a932563
stevenjmarsh <sjm.atl@gmail.com>
2016-12-19 18:19:46 -0500

    clearing up eslint error, per eslint, removing unnecessary braces of arrow function

--- imports/ui/components/Sidebar.jsx
+++ imports/ui/components/Sidebar.jsx
@@ -1,15 +1,14 @@
 import React from "react";
+import { Link } from "react-router";
 
-const Sidebar = () => {
-  return (
-    <div className="sidebar">
-      <div className="logo" />
-      <div className="pitch">
-        A platform to find, share, and discuss healthy places to shop and eat.
-      </div>
-      <div className="get-started">Get Started</div>
+const Sidebar = () =>
+  <div className="sidebar">
+    <Link to="/">Home</Link>
+    <div className="logo" />
+    <div className="pitch">
+      A platform to find, share, and discuss healthy places to shop and eat.
     </div>
-  );
-};
+    <div className="get-started">Get Started</div>
+  </div>;
 
 export default Sidebar;
