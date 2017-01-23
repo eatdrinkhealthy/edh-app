2f172bc404bcc2e6cc0d048a22405814a634d069
stevenjmarsh <sjm.atl@gmail.com>
2017-01-23 15:32:47 -0500

    adding initial test story for Filter component

--- imports/ui/components/__stories__/Filter.jsx
+++ imports/ui/components/__stories__/Filter.jsx
@@ -1,7 +1,27 @@
 import React from "react";
 import { storiesOf, action } from "@kadira/storybook";
 
-storiesOf("Button", module)
-.add("with text", () => (
-  <button onClick={action("clicked")}>Hello Button</button>
-));
+import Filter from "../Filter";
+
+const testFilterList = {
+  juiceBar1: {
+    name: "Juice Bars 1",
+    fourSquareCategory: "1",
+  },
+  juiceBar2: {
+    name: "Juice Bars 2",
+    fourSquareCategory: "2",
+  },
+  juiceBar3: {
+    name: "Juice Bars 3",
+    fourSquareCategory: "3",
+  },
+};
+
+storiesOf("Filter", module)
+  .add("with no filter list", () => (
+    <Filter />
+  ))
+  .add("with sample filter list", () => (
+    <Filter filterList={testFilterList} />
+  ));
