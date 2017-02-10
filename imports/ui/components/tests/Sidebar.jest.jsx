7da7fd7e1f30cbb36ff76907ad8321e9246216df
stevenjmarsh <sjm.atl@gmail.com>
2017-02-10 17:00:17 -0500

    adding snapshot test for Sidebar

---
+++ imports/ui/components/tests/Sidebar.jest.jsx
@@ -0,0 +1,14 @@
+/* eslint-env jest */
+/* eslint-disable func-names, prefer-arrow-callback, no-unused-expressions */
+/* eslint-disable import/no-extraneous-dependencies */
+
+import React from "react";
+import renderer from "react-test-renderer";
+import Sidebar from "../Sidebar";
+
+describe("<Sidebar />", function () {
+  it("matches render snapshot", function () {
+    const tree = renderer.create(<Sidebar />);
+    expect(tree).toMatchSnapshot();
+  });
+});
