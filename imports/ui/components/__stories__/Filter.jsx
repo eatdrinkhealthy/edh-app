f4095b6a7953dc817662a7f2b0b8f53c73010343
stevenjmarsh <sjm.atl@gmail.com>
2017-02-10 11:30:33 -0500

    correcting a couple eslint errors, and fixing broken storybook for Filter

--- imports/ui/components/__stories__/Filter.jsx
+++ imports/ui/components/__stories__/Filter.jsx
@@ -1,22 +1,27 @@
+/* eslint-disable import/no-extraneous-dependencies */
+
 import React from "react";
-import { storiesOf, action } from "@kadira/storybook";
+import { storiesOf } from "@kadira/storybook";
 
 import Filter from "../Filter";
 
-const testFilterList = {
-  juiceBar1: {
+const testFilterList = [
+  {
+    id: "juiceBar1",
     name: "Juice Bars 1",
     fourSquareCategory: "1",
   },
-  juiceBar2: {
+  {
+    id: "juiceBar2",
     name: "Juice Bars 2",
     fourSquareCategory: "2",
   },
-  juiceBar3: {
+  {
+    id: "juiceBar3",
     name: "Juice Bars 3",
     fourSquareCategory: "3",
   },
-};
+];
 
 storiesOf("Filter", module)
   .add("with no filter list", () => (
