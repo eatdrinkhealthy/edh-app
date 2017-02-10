5cc3a118f7f81ec4e89ccb3b9d893047ebd8ddf9
stevenjmarsh <sjm.atl@gmail.com>
2017-02-10 11:37:35 -0500

    clearing some eslint errors from tests

--- tests/jest-meteor-mock-tests/simple-schema.jest.js
+++ tests/jest-meteor-mock-tests/simple-schema.jest.js
@@ -1,5 +1,6 @@
 /* eslint-env jest */
 /* eslint-disable func-names, prefer-arrow-callback, no-unused-expressions, import/imports-first */
+/* eslint-disable import/no-extraneous-dependencies, import/extensions */
 
 import { SimpleSchema } from "meteor/aldeed:simple-schema";
 
