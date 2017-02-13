8c6d14d18bc1801efd851e3a0c7f534f221465cd
stevenjmarsh <sjm.atl@gmail.com>
2017-02-13 18:42:18 -0500

    adding snapshot for Navbar

--- imports/ui/components/Navbar.jsx
+++ imports/ui/components/Navbar.jsx
@@ -3,7 +3,6 @@ import { Link } from "react-router";
 
 const Navbar = () =>
   <div className="nav">
-
     <Link to="/sidebar">
       <div className="toggle-sidebar">
         <div className="burger" />
---
+++ imports/ui/components/tests/Navbar.jest.jsx
@@ -0,0 +1,14 @@
+/* eslint-env jest */
+/* eslint-disable func-names, prefer-arrow-callback, no-unused-expressions */
+/* eslint-disable import/no-extraneous-dependencies */
+
+import React from "react";
+import renderer from "react-test-renderer";
+import Navbar from "../Navbar";
+
+describe("<Navbar />", function () {
+  it("matches render snapshot", function () {
+    const tree = renderer.create(<Navbar />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
