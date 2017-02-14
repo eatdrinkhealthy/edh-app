63b83dca409a6a80538616e4a87616cd4724f77a
stevenjmarsh <sjm.atl@gmail.com>
2017-02-14 12:15:26 -0500

    adding storybook for PageNotFound

---
+++ imports/ui/pages/tests/__stories__/Pages.stories.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable import/no-extraneous-dependencies */
+
+import React from "react";
+import {
+  storiesOf,
+} from "@kadira/storybook";
+import PageNotFound from "../../PageNotFound";
+
+storiesOf("Pages", module)
+  .add("Page Not Found", () => (<PageNotFound />));
