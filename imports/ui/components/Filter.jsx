a61df6ece535b4a21a4c4244dd976c9adc175bf6
stevenjmarsh <sjm.atl@gmail.com>
2017-02-17 18:40:17 -0500

    adding flow type checking to Filter components, adjusting two eslint flow type rules

--- imports/ui/components/Filter.jsx
+++ imports/ui/components/Filter.jsx
@@ -1,23 +1,30 @@
 // @flow
-import React, {
-  PropTypes,
-} from "react";
+import React from "react";
 import { Link } from "react-router";
 import Toggle from "react-toggle";
 
-type FilterItemType = {
+type FilterItemPropTypes = {
   label: string,
   filterId: string,
 };
 
-const FilterItem = ({ label, filterId }: FilterItemType): React$Component => (
+const FilterItem = ({ label, filterId }: FilterItemPropTypes) => (
   <div>
     <span className="react-toggle-label">{label}</span>
     <span className="react-toggle"> <Toggle id={filterId} /></span>
   </div>
 );
 
-const Filter = ({ filterList = [] }) => (
+type FilterType = {
+  name: string,  // eslint-disable-line react/no-unused-prop-types
+  id: string,    // eslint-disable-line react/no-unused-prop-types
+};
+
+type FilterPropTypes = {
+  filterList: FilterType[],
+};
+
+const Filter = ({ filterList = [] }: FilterPropTypes) => (
   <div>
     <div className="filter-header">
       <h4>Filter</h4>
@@ -26,7 +33,7 @@ const Filter = ({ filterList = [] }) => (
       </Link>
     </div>
     <div className="filter">
-      {filterList.map(filter =>
+      {filterList.map((filter: FilterType) =>
         (<FilterItem
           key={filter.id}
           label={filter.name}
@@ -35,13 +42,6 @@ const Filter = ({ filterList = [] }) => (
     </div>
   </div>
 );
-
-Filter.propTypes = {
-  filterList: PropTypes.arrayOf(PropTypes.shape({
-    id: PropTypes.string.isRequired,
-    name: PropTypes.string.isRequired,
-  })),
-};
 
 export default Filter;
 
