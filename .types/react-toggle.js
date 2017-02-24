12a25c0c09623b7d9457d1b081e18a5d7869c006
stevenjmarsh <sjm.atl@gmail.com>
2017-02-24 11:09:12 -0500

    correcting libdef for react-toggle, now enforces proptypes. resolves # 26

--- .types/react-toggle.js
+++ .types/react-toggle.js
@@ -1,17 +1,16 @@
 // Hand creating react-toggle libdef
 // props and types listed at https://github.com/aaronshaf/react-toggle
-// TODO confirm (or fix), props typechecking is enforced
-type ReactToggle$Props = {
-  checked?: boolean,
-  defaultChecked?: boolean,
-  onChange?: Function,
-  name?: string,
-  id?: string,
-  disabled?: boolean,
-};
-
 declare module "react-toggle" {
-  declare export default class Toggle extends React$Component<any, any, any> {
-    props: ReactToggle$Props;
+  declare type ReactToggleProps = {
+    checked?: boolean,
+    defaultChecked?: boolean,
+    onChange?: Function,
+    name?: string,
+    id: string,
+    disabled?: boolean,
+  };
+
+  declare export default class Toggle extends React$Component {
+    props: ReactToggleProps;
   }
 }
