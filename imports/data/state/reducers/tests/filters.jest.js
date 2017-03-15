3b161d529a970b1bc22e2b957c0f878cc9e1fab0
stevenjmarsh <sjm.atl@gmail.com>
2017-03-15 10:33:19 -0400

    adding more reducer tests

--- imports/data/state/reducers/tests/filters.jest.js
+++ imports/data/state/reducers/tests/filters.jest.js
@@ -4,16 +4,82 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import filtersReducer, { toggleFilter } from "../filters";
 import DEFAULT_FILTER_LIST from "../../data/defaultFilters";
+import { toggleFilter as toggleFilterAction } from "../../actions/actionCreators";
 
 describe("filters reducer", function () {
+  const unknownAction = { type: "unknown", id: "a" };
+
   it("should return an initial state of DEFAULT_FILTER_LIST", function () {
-    const unknownAction = { type: "unknown", id: "a" };
     const initialState = filtersReducer(undefined, unknownAction);
 
     expect(initialState.filters).toEqual(DEFAULT_FILTER_LIST);
   });
 
-  it.skip("should return the previous state for any unknown action", function () {
+  it("should return the previous state for any unknown action", function () {
+    const previousState = {
+      filters: [
+        {
+          id: "1",
+          name: "Juice Bar 1",
+          on: true,
+          fourSquareCategory: "abc",
+        },
+      ],
+    };
+
+    const nextState = filtersReducer(previousState, unknownAction);
+    expect(nextState).toEqual(previousState);
+  });
+
+  it("should handle TOGGLE_FILTER action", function () {
+    const previousState = {
+      filters: [
+        {
+          id: "1",
+          name: "Juice Bar 1",
+          on: true,
+          fourSquareCategory: "abc",
+        },
+        {
+          id: "2",
+          name: "Juice Bar 2",
+          on: false,
+          fourSquareCategory: "def",
+        },
+        {
+          id: "3",
+          name: "Juice Bar 3",
+          on: false,
+          fourSquareCategory: "ghi",
+        },
+      ],
+    };
+    const goodResult = {
+      filters: [
+        {
+          id: "1",
+          name: "Juice Bar 1",
+          on: true,
+          fourSquareCategory: "abc",
+        },
+        {
+          id: "2",
+          name: "Juice Bar 2",
+          on: false,
+          fourSquareCategory: "def",
+        },
+        {
+          id: "3",
+          name: "Juice Bar 3",
+          on: true,
+          fourSquareCategory: "ghi",
+        },
+      ],
+    };
+    const toggleAction = toggleFilterAction("3");
+    const nextState = filtersReducer(previousState, toggleAction);
+
+    expect(nextState).toEqual(goodResult);
   });
 
   describe("toggleFilter function", function () {
@@ -39,12 +105,35 @@ describe("filters reducer", function () {
         },
       ],
     };
+    const copyState = { ...previousState };
+    const newState = toggleFilter(previousState, "2");
 
-    it("should toggle the 'on' property of indicated filter", function () {
-      const copyState = { ...previousState };
-      const newState = toggleFilter(previousState, "2");
+    it("should only toggle the 'on' property of indicated filter", function () {
+      expect(newState).toEqual({
+        filters: [
+          {
+            id: "1",
+            name: "Juice Bar 1",
+            on: true,
+            fourSquareCategory: "abc",
+          },
+          {
+            id: "2",
+            name: "Juice Bar 2",
+            on: true,
+            fourSquareCategory: "def",
+          },
+          {
+            id: "3",
+            name: "Juice Bar 3",
+            on: false,
+            fourSquareCategory: "ghi",
+          },
+        ],
+      });
+    });
 
-      expect(newState.filters[1].on).toBe(true);        // correctly assigns new state
+    it("should not mutate previous state", function () {
       expect(previousState).toEqual(copyState);  // does not mutate previous state
     });
   });
