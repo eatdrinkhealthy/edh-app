f357b06a005b363dde94218110cb7940469f7f42
stevenjmarsh <sjm.atl@gmail.com>
2017-03-16 13:08:33 -0400

    passing initial dispatch handler to FilterItem component

--- imports/ui/components/Filter.jsx
+++ imports/ui/components/Filter.jsx
@@ -1,5 +1,5 @@
 // @flow
-import React from "react";
+import React, { Component } from "react";
 import { Link } from "react-router";
 import Toggle from "react-toggle";
 import type { IFilter, IFilterList } from "../../data/state/data/defaultFiltersTypes";
@@ -7,20 +7,40 @@ import type { IFilter, IFilterList } from "../../data/state/data/defaultFiltersT
 type IFilterItemProps = {
   label: string,
   filterId: string,
+  filterOn: boolean,
+  setFilterHandler: () => void,
 };
 
-const FilterItem = ({ label, filterId }: IFilterItemProps) => (
-  <div>
-    <span className="react-toggle-label">{label}</span>
-    <span className="react-toggle"><Toggle id={filterId} /></span>
-  </div>
-);
+class FilterItem extends Component {
+  handleToggleChange(event) {
+    console.log("checked:", event.target.id, event.target.checked);
+    // this.props.setFilterHandler("abc");
+  }
+
+  props: IFilterItemProps;
+
+  render() {
+    return (
+      <div>
+        <span className="react-toggle-label">{this.props.label}</span>
+        <span className="react-toggle">
+          <Toggle
+            id={this.props.filterId}
+            checked={this.props.filterOn}
+            onChange={this.handleToggleChange}
+          />
+        </span>
+      </div>
+    );
+  }
+}
 
 type IFilterProps = {
   filterList?: IFilterList,
+  setFilterHandler: () => void,
 };
 
-const Filter = ({ filterList = [] }: IFilterProps) => (
+const Filter = ({ filterList = [], setFilterHandler }: IFilterProps) => (
   <div>
     <div className="filter-header">
       <h4>Filter</h4>
@@ -34,6 +54,8 @@ const Filter = ({ filterList = [] }: IFilterProps) => (
           key={filter.id}
           label={filter.name}
           filterId={filter.id}
+          filterOn={filter.on}
+          setFilterHandler={setFilterHandler}
         />))}
     </div>
   </div>
--- imports/ui/containers/FilterContainer.jsx
+++ imports/ui/containers/FilterContainer.jsx
@@ -1,5 +1,6 @@
 // @flow
 import { connect } from "react-redux";
+import toggleFilter from "../../data/state/actions/actionCreators";
 import Filter from "../components/Filter";
 import type { IState } from "../../data/state/reducers/filters";
 
@@ -7,6 +8,10 @@ const mapStateToProps = (state: IState) => ({
   filterList: state.filters,
 });
 
-const FilterContainer = connect(mapStateToProps)(Filter);
+const mapDispatchToProps = dispatch => ({
+  setFilterHandler: (id: string) => (dispatch(toggleFilter(id))),
+});
+
+const FilterContainer = connect(mapStateToProps, mapDispatchToProps)(Filter);
 
 export default FilterContainer;
