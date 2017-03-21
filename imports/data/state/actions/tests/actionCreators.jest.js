5f5ee3618bbe356cb4b385244d7a86c4cb769b54
stevenjmarsh <sjm.atl@gmail.com>
2017-03-21 14:20:20 -0400

    refactor, renaming setFilter import

--- imports/data/state/actions/tests/actionCreators.jest.js
+++ imports/data/state/actions/tests/actionCreators.jest.js
@@ -1,21 +1,19 @@
 /* eslint-env jest */
 /* eslint-disable func-names, prefer-arrow-callback, no-unused-expressions */
 /* eslint-disable import/no-extraneous-dependencies */
-import { setFilter as setFilterActionCreator } from "../actionCreators";
+import * as actions from "../actionCreators";
 import SET_FILTER from "../actionTypes";
 
 describe("actions", function () {
   it("should create a SET_FILTER action", function () {
     const id = "abc";
     const checked = true;
-    const setFilterAction = setFilterActionCreator(id, checked);
     const expectedAction = {
       type: SET_FILTER,
       id,
       checked,
     };
 
-    expect(setFilterAction).toEqual(expectedAction);
+    expect(actions.setFilter(id, checked)).toEqual(expectedAction);
   });
 });
-
