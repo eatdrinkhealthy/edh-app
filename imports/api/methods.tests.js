8edb121470439668ed855edea73e48eb4525c542
stevenjmarsh <sjm.atl@gmail.com>
2017-03-30 19:57:24 -0400

    adding initial working method integration test

--- imports/api/methods.tests.js
+++ imports/api/methods.tests.js
@@ -3,12 +3,20 @@
 /* eslint-disable func-names, prefer-arrow-callback */
 import { assert } from "meteor/practicalmeteor:chai";
 import { Meteor } from "meteor/meteor";
+import { getNearbyPlaces } from "./methods";
 
 if (Meteor.isServer) {
   describe("methods", function () {
     describe("getNearbyPlaces", function () {
-      it.skip("should list this test", function () {
-        assert.equal(1, 1);
+      it("should return a list of the foursquare category codes [UPDATE]", function () {
+        // TODO this test will be obsolete once the method begins to return real data
+        const context = {};
+        const args = {
+        };
+
+        assert.throws(() => {
+          getNearbyPlaces._execute(context, args);
+        }, Meteor.Error, /validation-error/);
       });
     });
   });
