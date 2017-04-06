07d21409404a52d039bf4b82e6b03dab62aff2c9
stevenjmarsh <sjm.atl@gmail.com>
2017-04-06 18:15:31 -0400

    adding searchResults actions

--- imports/data/state/actions/actionCreators.js
+++ imports/data/state/actions/actionCreators.js
@@ -1,5 +1,10 @@
 // @flow
-import { SET_FILTER } from "./actionTypes";
+import { SET_FILTER, SET_SEARCH_RESULTS } from "./actionTypes";
+import type { IFoursquareVenue } from "../../../api/foursquare/foursquareApi";
+
+//
+// Filter Actions
+//
 
 export type IFilterAction = {
   type: string,
@@ -7,9 +12,28 @@ export type IFilterAction = {
   checked: boolean,
 };
 
-// eslint-disable-next-line import/prefer-default-export
-export const setFilter = (id: string, checked: boolean): IFilterAction => ({
+export const setFilter = (
+  id: string,
+  checked: boolean,
+): IFilterAction => ({
   type: SET_FILTER,
   id,
   checked,
+});
+
+
+//
+// Search Results Actions
+//
+
+export type ISearchResultsAction = {
+  type: string,
+  searchResults: Array<IFoursquareVenue>,
+};
+
+export const setSearchResults = (
+  searchResults: Array<IFoursquareVenue>,
+): ISearchResultsAction => ({
+  type: SET_SEARCH_RESULTS,
+  searchResults,
 });
--- imports/data/state/actions/tests/actionCreators.jest.js
+++ imports/data/state/actions/tests/actionCreators.jest.js
@@ -1,19 +1,51 @@
+// @flow
 /* eslint-env jest */
 /* eslint-disable func-names, prefer-arrow-callback, no-unused-expressions */
 /* eslint-disable import/no-extraneous-dependencies */
 import * as actions from "../actionCreators";
-import { SET_FILTER } from "../actionTypes";
+import { SET_FILTER, SET_SEARCH_RESULTS } from "../actionTypes";
 
 describe("actions", function () {
-  it("should create a SET_FILTER action", function () {
-    const id = "abc";
-    const checked = true;
-    const expectedAction = {
-      type: SET_FILTER,
-      id,
-      checked,
-    };
+  describe("setFilter", function () {
+    it("should create a SET_FILTER action", function () {
+      const id = "abc";
+      const checked = true;
+      const expectedAction = {
+        type: SET_FILTER,
+        id,
+        checked,
+      };
 
-    expect(actions.setFilter(id, checked)).toEqual(expectedAction);
+      expect(actions.setFilter(id, checked)).toEqual(expectedAction);
+    });
+  });
+
+  describe("setSearchResults", function () {
+    it("should create a SET_SEARCH_RESULTS action, with no search results", function () {
+      const expectedAction = {
+        type: SET_SEARCH_RESULTS,
+        searchResults: [],
+      };
+
+      expect(actions.setSearchResults([])).toEqual(expectedAction);
+    });
+
+    it("should create a SET_SEARCH_RESULTS action, with search results", function () {
+      const searchResults = [
+        { id: "1", name: "cafe1", location: { lat: 32.7842149282925, lng: -79.93963580270426 } },
+        { id: "2", name: "cafe2", location: { lat: 32.7842149282936, lng: -79.93963580270437 } },
+        { id: "3", name: "cafe3", location: { lat: 32.7842149282947, lng: -79.93963580270448 } },
+      ];
+      const expectedAction = {
+        type: SET_SEARCH_RESULTS,
+        searchResults: [
+          { id: "1", name: "cafe1", location: { lat: 32.7842149282925, lng: -79.93963580270426 } },
+          { id: "2", name: "cafe2", location: { lat: 32.7842149282936, lng: -79.93963580270437 } },
+          { id: "3", name: "cafe3", location: { lat: 32.7842149282947, lng: -79.93963580270448 } },
+        ],
+      };
+
+      expect(actions.setSearchResults(searchResults)).toEqual(expectedAction);
+    });
   });
 });
