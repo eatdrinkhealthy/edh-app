3d53eb04dc8a8505ddad72c2e4d0e69b16d04aa9
stevenjmarsh <sjm.atl@gmail.com>
2017-04-14 16:52:46 -0400

    correcting LocationsMap storybook, now displays markers again

--- imports/ui/components/tests/__stories__/LocationsMap.stories.jsx
+++ imports/ui/components/tests/__stories__/LocationsMap.stories.jsx
@@ -4,23 +4,22 @@
 import React from "react";
 import { storiesOf } from "@kadira/storybook";
 import LocationsMap from "../../LocationsMap";
-import Marker from "../../Marker";
 
 /* global Window */
+
+const sampleVenues = [
+  { id: "A", name: "testVenueA", location: { lat: 32.789008, lng: -79.932115 } },
+  { id: "B", name: "testVenueB", location: { lat: 32.789659, lng: -79.935796 } },
+  { id: "C", name: "testVenueC", location: { lat: 32.785699, lng: -79.935796 } },
+];
 
 storiesOf("Map", module)
   .add("LocationsMap - no markers", (): React$Element<*> => (
     <LocationsMap googleMapsApiKey={Window.Meteor.settings.public.googleMapsApiKey} />
   ))
-  .add("LocationsMap - one center marker", (): React$Element<*> => (
-    <LocationsMap googleMapsApiKey={Window.Meteor.settings.public.googleMapsApiKey} >
-      <Marker lat={32.789008} lng={-79.932115} />
-    </LocationsMap>
-  ))
-.add("LocationsMap - multiple markers", (): React$Element<*> => (
-  <LocationsMap googleMapsApiKey={Window.Meteor.settings.public.googleMapsApiKey} >
-    <Marker lat={32.789008} lng={-79.932115} />
-    <Marker lat={32.789659} lng={-79.935796} />
-    <Marker lat={32.785699} lng={-79.935796} />
-  </LocationsMap>
-));
+  .add("LocationsMap - multiple markers", (): React$Element<*> => (
+    <LocationsMap
+      googleMapsApiKey={Window.Meteor.settings.public.googleMapsApiKey}
+      venues={sampleVenues}
+    />
+  ));
