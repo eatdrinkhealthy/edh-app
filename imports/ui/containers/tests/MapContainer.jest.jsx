5d8c9b62428b815ee3cb2a144ac54b0f6b6fbcc5
stevenjmarsh <sjm.atl@gmail.com>
2017-04-18 15:57:59 -0400

    adding tests for MapContainer

--- imports/ui/containers/tests/MapContainer.jest.jsx
+++ imports/ui/containers/tests/MapContainer.jest.jsx
@@ -3,14 +3,27 @@
 /* eslint-disable func-names, prefer-arrow-callback, no-unused-expressions */
 /* eslint-disable import/no-extraneous-dependencies */
 import React from "react";
-import {
-  shallow,
-} from "enzyme";
+import { Provider } from "react-redux";
+import { shallow, mount } from "enzyme";
 import toJson from "enzyme-to-json";
-import {
-  MapComponent,
-} from "../MapContainer";
+import { createStore } from "redux";
+import appReducer from "../../../data/state/reducers";
+import MapContainer, { MapComponent } from "../MapContainer";
+
+/* eslint-disable no-duplicate-imports */
 import type { IFilter } from "../../../data/state/data/defaultFilters";
+import type { IState } from "../../../data/state/stores/store";
+/* eslint-enable no-duplicate-imports */
+
+import { getNearbyPlaces } from "../../../api/methods";
+
+// mock getNearbyPlaces (a method call for foursquare api)
+// eslint-disable-next-line flowtype/require-return-type
+jest.mock("../../../api/methods", () => ({
+  getNearbyPlaces: {
+    call: jest.fn(),
+  },
+}));
 
 describe("<MapComponent />", function () {
   const testFilterList: Array<IFilter> = [
@@ -33,7 +46,43 @@ describe("<MapComponent />", function () {
 });
 
 describe("<MapContainer />", function () {
-  it.skip("should take state from store and provide to MapComponent", function () {
-    // likely an unnecessary test.
+  const testDefaultState: IState = {
+    filters: [
+      { id: "juiceBar", name: "Juice Bar", on: true, foursquareCategory: "1" },
+      { id: "cafe", name: "Cafe", on: false, foursquareCategory: "2" },
+      { id: "market", name: "Market", on: false, foursquareCategory: "3" },
+    ],
+    searchResults: [
+      { id: "A", name: "testVenueA", location: { lat: 32.789008, lng: -79.932115 } },
+      { id: "B", name: "testVenueB", location: { lat: 32.789659, lng: -79.935796 } },
+      { id: "C", name: "testVenueC", location: { lat: 32.785699, lng: -79.935796 } },
+    ],
+    mapDisplay: { selectedVenueId: "B" },
+  };
+
+  const testStore = createStore(
+    appReducer,
+    testDefaultState,
+  );
+
+  const wrapper = mount(
+    <Provider store={testStore}>
+      <MapContainer />
+    </Provider>,
+  );
+
+  it("should call getNearbyPlaces method with selected filters", function () {
+    expect(getNearbyPlaces.call.mock.calls[0][0].filterList)
+      .toEqual([{ id: "juiceBar", name: "Juice Bar", on: true, foursquareCategory: "1" }]);
+  });
+
+  it("should set search results for MapComponent from state", function () {
+    expect(wrapper.find("MapComponent").at(0).props().searchResults)
+      .toEqual(testDefaultState.searchResults);
+  });
+
+  it("should set selectedVenueId from state", function () {
+    expect(wrapper.find("MapComponent").at(0).props().selectedVenueId)
+      .toEqual(testDefaultState.mapDisplay.selectedVenueId);
   });
 });
