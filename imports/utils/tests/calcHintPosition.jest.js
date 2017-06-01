20254fe80697dbbc4f9c4fbab74725497851169c
stevenjmarsh <sjm.atl@gmail.com>
2017-06-01 16:36:27 -0400

    skipping incomplete calcHintPosition tests for now

--- imports/utils/tests/calcHintPosition.jest.js
+++ imports/utils/tests/calcHintPosition.jest.js
@@ -13,21 +13,21 @@ describe("calcHintPosition", function () {
     expect(calcHintPosition({ top: 0, right: 0, bottom: 0, left: 0 }, null)).toBe("hint--bottom");
   });
 
-  it("should return 'hint--top' if hint bottom is past hint view bottom", function () {
+  it.skip("should return 'hint--top' if hint bottom is past hint view bottom", function () {
     const hintViewArea = { top: 10, right: 600, bottom: 600, left: 10 };
     const hintArea = { top: 20, right: 120, bottom: 601, left: 20 };
 
     expect(calcHintPosition(hintViewArea, hintArea)).toBe("hint--top");
   });
 
-  it("should return 'hint--left' if hint right is past hint view right", function () {
+  it.skip("should return 'hint--left' if hint right is past hint view right", function () {
     const hintViewArea = { top: 10, right: 600, bottom: 600, left: 10 };
     const hintArea = { top: 20, right: 120, bottom: 80, left: 20 };
 
     expect(calcHintPosition(hintViewArea, hintArea)).toBe("hint--left");
   });
 
-  it("should return 'hint--right' if hint left is past hint view left", function () {
+  it.skip("should return 'hint--right' if hint left is past hint view left", function () {
     const hintViewArea = { top: 10, right: 600, bottom: 600, left: 10 };
     const hintArea = { top: 20, right: 120, bottom: 80, left: 5 };
 
