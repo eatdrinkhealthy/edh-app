715be414000e1d3890df392ea2e273309d927964
stevenjmarsh <sjm.atl@gmail.com>
2017-06-02 13:30:25 -0400

    adding initial logic for calcHintPosition, and updating tests (wip)

--- imports/utils/calcHintPosition.js
+++ imports/utils/calcHintPosition.js
@@ -8,8 +8,37 @@ const calcHintPosition = (
   let hintPos = "hint--bottom";
 
   if (hintViewArea && hintArea) {
-    hintPos = "hint--top";
+    const fitsTop = hintArea.top > hintViewArea.top;
+    const fitsRight = hintArea.right < hintViewArea.right;
+    const fitsBottom = hintArea.bottom < hintViewArea.bottom;
+    const fitsLeft = hintArea.left > hintViewArea.left;
+
+    const fitsAnywhere = fitsTop && fitsRight && fitsBottom && fitsLeft;
+    const fitsNowhere = !fitsTop && !fitsRight && !fitsBottom && !fitsLeft;
+
+    hintPos = "hint-";
+
+    if (fitsAnywhere || fitsNowhere) {
+      hintPos += "-bottom";
+    } else {
+      if (fitsBottom) {
+        hintPos += "-bottom";
+      } else if (fitsTop) {
+        hintPos += "-top";
+      }
+
+      if (fitsRight) {
+        hintPos += "-right";
+      } else if (fitsLeft) {
+        hintPos += "-left";
+      }
+    }
+
+    if (hintPos.endsWith("-")) {
+      hintPos = "hint--bottom";     // if missed any cases, set to bottom
+    }
   }
+
   return hintPos;
 };
 
--- imports/utils/tests/calcHintPosition.jest.js
+++ imports/utils/tests/calcHintPosition.jest.js
@@ -13,24 +13,59 @@ describe("calcHintPosition", function () {
     expect(calcHintPosition({ top: 0, right: 0, bottom: 0, left: 0 }, null)).toBe("hint--bottom");
   });
 
-  it.skip("should return 'hint--top' if hint bottom is past hint view bottom", function () {
+  it("should return 'hint--top' if hint bottom >= hint view bottom, and fits on top", function () {
     const hintViewArea = { top: 10, right: 600, bottom: 600, left: 10 };
     const hintArea = { top: 20, right: 120, bottom: 601, left: 20 };
 
     expect(calcHintPosition(hintViewArea, hintArea)).toBe("hint--top");
   });
 
-  it.skip("should return 'hint--left' if hint right is past hint view right", function () {
+  it("should return 'hint--bottom' if hint bottom >= hint view bottom, and doesn't fit top", function () {
     const hintViewArea = { top: 10, right: 600, bottom: 600, left: 10 };
-    const hintArea = { top: 20, right: 120, bottom: 80, left: 20 };
+    const hintArea = { top: 9, right: 120, bottom: 601, left: 20 };
+
+    expect(calcHintPosition(hintViewArea, hintArea)).toBe("hint--bottom");
+  });
+
+  it("should return 'hint--left' if hint right >= hint view right, and fits left", function () {
+    const hintViewArea = { top: 10, right: 600, bottom: 600, left: 10 };
+    const hintArea = { top: 20, right: 601, bottom: 80, left: 20 };
 
     expect(calcHintPosition(hintViewArea, hintArea)).toBe("hint--left");
   });
 
-  it.skip("should return 'hint--right' if hint left is past hint view left", function () {
+  it("should return 'hint--bottom' if hint right is past hint view right, and doesn't fit left", function () {
+    const hintViewArea = { top: 10, right: 110, bottom: 600, left: 10 };
+    const hintArea = { top: 20, right: 120, bottom: 80, left: 20 };
+
+    expect(calcHintPosition(hintViewArea, hintArea)).toBe("hint--bottom");
+  });
+
+  it("should return 'hint--right' if hint left is past hint view left, and fits right", function () {
     const hintViewArea = { top: 10, right: 600, bottom: 600, left: 10 };
     const hintArea = { top: 20, right: 120, bottom: 80, left: 5 };
 
     expect(calcHintPosition(hintViewArea, hintArea)).toBe("hint--right");
+  });
+
+  it("should return 'hint--bottom' if hint left is past hint view left, and doesn't fit right", function () {
+    const hintViewArea = { top: 10, right: 600, bottom: 600, left: 10 };
+    const hintArea = { top: 20, right: 120, bottom: 80, left: 5 };
+
+    expect(calcHintPosition(hintViewArea, hintArea)).toBe("hint--bottom");
+  });
+
+  it("should return 'hint--bottom' if hint top is past hint view top, and fits bottom", function () {
+    const hintViewArea = { top: 10, right: 600, bottom: 600, left: 10 };
+    const hintArea = { top: 20, right: 120, bottom: 80, left: 5 };
+
+    expect(calcHintPosition(hintViewArea, hintArea)).toBe("hint--bottom");
+  });
+
+  it("should return 'hint--bottom' if hint top is past hint view top, and DOESN'T fit bottom", function () {
+    const hintViewArea = { top: 10, right: 600, bottom: 600, left: 10 };
+    const hintArea = { top: 20, right: 120, bottom: 80, left: 5 };
+
+    expect(calcHintPosition(hintViewArea, hintArea)).toBe("hint--bottom");
   });
 });
