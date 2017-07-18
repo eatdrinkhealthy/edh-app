5955c9d27ce6c104a500445fcd405c2e0942fd7d
stevenjmarsh <sjm.atl@gmail.com>
2017-07-18 13:20:44 -0400

    adding LandingPage section in smoke test

--- tests/end-to-end/smokeTest_spec.js
+++ tests/end-to-end/smokeTest_spec.js
@@ -6,8 +6,10 @@
 const baseUrl = "http://localhost:3000";
 
 describe("Smoke Test", function () {
-  it("displays the map and navbar as the main page", function () {
-    browser.url(baseUrl);
-    expect(browser.getTitle()).toEqual("Eat Drink Healthy");
+  describe("Landing Page", function () {
+    it("displays the map and navbar as the main page", function () {
+      browser.url(baseUrl);
+      expect(browser.getTitle()).toEqual("Eat Drink Healthy");
+    });
   });
 });
