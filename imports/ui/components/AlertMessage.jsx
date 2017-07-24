041d5157ac5cb839499b531feaf1abca98183f30
stevenjmarsh <sjm.atl@gmail.com>
2017-07-24 14:18:20 -0400

    switching from bastardized composition, to inheritance of Alert class

--- imports/ui/components/AlertMessage.jsx
+++ imports/ui/components/AlertMessage.jsx
@@ -1,31 +1,21 @@
 // deliberately not flow checking this file (get working without it first!)
-import React from "react";
 import PropTypes from "prop-types";
 import Alert from "react-s-alert";
 
 // note, css required for this package is imported in 'importPackagesCss.js'
 
-const AlertMessage = props => (
-  <Alert {...props} />
-);
+class AlertMessage extends Alert {}
 
 AlertMessage.propTypes = {
   position: PropTypes.string,
   offset: PropTypes.number,
 };
 
+// customizing default props (slightly different than Alert)
 AlertMessage.defaultProps = {
   position: "top-left",
   offset: 70,
 };
-
-// assign the Alert component methods to the functional component
-AlertMessage.warning = Alert.warning;
-AlertMessage.error = Alert.error;
-AlertMessage.info = Alert.info;
-AlertMessage.success = Alert.success;
-AlertMessage.close = Alert.close;
-AlertMessage.closeAll = Alert.closeAll;
 
 export default AlertMessage;
 
