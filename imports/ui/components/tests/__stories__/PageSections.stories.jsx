a6883db1f329363b99668aa791b7b50fc8d5fada
stevenjmarsh <sjm.atl@gmail.com>
2017-07-26 17:50:37 -0400

    adding MemoryRouter to fix displaying Navbar component in storybook

--- imports/ui/components/tests/__stories__/PageSections.stories.jsx
+++ imports/ui/components/tests/__stories__/PageSections.stories.jsx
@@ -1,12 +1,15 @@
 // @flow
 /* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable flowtype/require-parameter-type, flowtype/require-return-type */
 
 import React from "react";
+import { MemoryRouter } from "react-router-dom";
 import { storiesOf } from "@storybook/react";
 
 import Navbar from "../../Navbar";
 
 storiesOf("Page Sections", module)
+  .addDecorator(story => (<MemoryRouter>{story()}</MemoryRouter>))
   .add("Navbar", (): React$Element<*> => (
     <Navbar />
   ));
