77e83ff107d26de3ba8cbe5d36bfebcb1e424214
stevenjmarsh <sjm.atl@gmail.com>
2017-07-27 15:46:58 -0400

    making sidebar smaller, removing logo

--- imports/ui/components/Sidebar.jsx
+++ imports/ui/components/Sidebar.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 const Sidebar = (): React$Element<*> => (
   <div className="sidebar">
     <Link to="/">Home</Link>
-    <div className="logo" />
+    <h2 className="mh4">Eat Drink Healthy</h2>
     <div className="pitch">
       A platform to find, share, and discuss healthy places to shop and eat.
     </div>
