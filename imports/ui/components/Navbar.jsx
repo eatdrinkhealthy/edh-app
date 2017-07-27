2fd37a3826447e9a5e7b605a04b9404c2abee5a1
stevenjmarsh <sjm.atl@gmail.com>
2017-07-27 15:41:28 -0400

    changing Navbar to use buttons, removing hamburger

--- imports/ui/components/Navbar.jsx
+++ imports/ui/components/Navbar.jsx
@@ -4,19 +4,13 @@ import { Link } from "react-router-dom";
 
 const Navbar = (): React$Element<*> => (
   <div className="nav">
-    <Link to="/sidebar">
-      <div className="toggle-sidebar">
-        <div className="burger" />
-        <div className="burger" />
-        <div className="burger" />
+    <div className="dt w-100 pa2">
+      <div className="dtc v-mid white w-25">Eat Drink Healthy</div>
+      <div className="dtc v-mid w-75 tr">
+        <Link className="f6 link dim ma1 ph3 pv2 mb2 dib white bg-purple" to="/sidebar">Join</Link>
+        <Link className="f6 link dim ma1 ph3 pv2 mb2 dib white bg-dark-green" to="/filter">Filter</Link>
       </div>
-    </Link>
-
-    <div className="nav__title">Eat Drink Healthy</div>
-
-    <Link to="/filter">
-      <div className="toggle-filter">Filter</div>
-    </Link>
+    </div>
   </div>
 );
 
