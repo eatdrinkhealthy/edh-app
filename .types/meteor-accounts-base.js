38bdaf6ebce0e58d5246231352004a9e33846636
stevenjmarsh <sjm.atl@gmail.com>
2017-07-31 14:52:01 -0400

    adding functions to Accounts (meteor/accounts-base) libdef: createUser, validateNewUser

--- .types/meteor-accounts-base.js
+++ .types/meteor-accounts-base.js
@@ -1,4 +1,15 @@
 // Hand creating meteor accounts base libdef
 declare module "meteor/accounts-base" {
-  declare var exports: any;
+  declare export var Accounts: {
+    createUser(
+      options: {
+        username: string,
+        email: string,
+        password: string,
+        profile?: {},
+      },
+      callback?: Function,
+    ): void,
+    validateNewUser({}): boolean,
+  }
 }
