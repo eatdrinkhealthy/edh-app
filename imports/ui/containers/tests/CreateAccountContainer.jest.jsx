0433a7b15b60890e383357980a861cff9c12b6c6
stevenjmarsh <sjm.atl@gmail.com>
2017-08-09 12:25:21 -0400

    adding CreateAccountContainer test for create account success message

--- imports/ui/containers/tests/CreateAccountContainer.jest.jsx
+++ imports/ui/containers/tests/CreateAccountContainer.jest.jsx
@@ -3,7 +3,25 @@
 /* eslint-disable func-names, prefer-arrow-callback, no-unused-expressions */
 /* eslint-disable import/no-extraneous-dependencies, import/first */
 
-import { lookupErrorMessage } from "../CreateAccountContainer";
+/* eslint-disable flowtype/require-return-type, flowtype/require-parameter-type */
+jest.mock("../../components/AlertMessage", () => (
+  class AlertMessage {
+    static success = jest.fn();
+  }
+));
+
+// NOTE, this mock overrides a global simpler mock for accounts-base
+jest.mock("meteor/accounts-base", () => ({
+  Accounts: {
+    createUser: jest.fn(({ username, email, password }, callback) => callback()),
+  },
+}));
+/* eslint-enable flowtype/require-return-type, flowtype/require-parameter-type */
+
+import React from "react";
+import { mount } from "enzyme";
+import CreateAccountContainer, { lookupErrorMessage } from "../CreateAccountContainer";
+import AlertMessage from "../../components/AlertMessage";
 
 describe("lookupErrorMessage", function () {
   it("should return a default message when can't match error", function () {
@@ -24,5 +42,35 @@ describe("lookupErrorMessage", function () {
   it("should match error message for email already exists", function () {
     expect(lookupErrorMessage("Email already exists."))
       .toEqual("An account with this email address already exists.");
+  });
+});
+
+describe("<CreateAccountContainer />", function () {
+  it("should call AlertMessage.success when Accounts.createUser is successful", function () {
+    const wrapper = mount(<CreateAccountContainer />);
+
+    const usernameInput = wrapper.find("[name='username']");
+    const emailInput = wrapper.find("[name='email']");
+    const passwordInput = wrapper.find("[name='password']");
+    const confirmPasswordInput = wrapper.find("[name='confirmPassword']");
+
+    // $FlowFixMe
+    usernameInput.get(0).value = "user12";
+    usernameInput.simulate("change", usernameInput);
+
+    // $FlowFixMe
+    emailInput.get(0).value = "user12@test.com";
+    emailInput.simulate("change", emailInput);
+
+    // $FlowFixMe
+    passwordInput.get(0).value = "user12pw";
+    passwordInput.simulate("change", passwordInput);
+
+    // $FlowFixMe
+    confirmPasswordInput.get(0).value = "user12pw";
+    confirmPasswordInput.simulate("change", confirmPasswordInput);
+
+    wrapper.find("input[type='submit']").simulate("submit");
+    expect(AlertMessage.success).toHaveBeenCalledWith("Welcome user12!");
   });
 });
