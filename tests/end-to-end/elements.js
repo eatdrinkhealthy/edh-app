2abf124c3307651a2c038eaeb2943b16b59b3b61
stevenjmarsh <sjm.atl@gmail.com>
2017-08-11 17:29:54 -0400

    corrected recently broken e-to-e tests, two more to fix

---
+++ tests/end-to-end/elements.js
@@ -0,0 +1,16 @@
+const elements = {
+  baseUrl: "http://localhost:3000",
+  locationsMapComponent: "div.map-container",
+  navbarComponent: "div.nav a[href='/sidebar']",
+  hamburger: ".toggle-sidebar",
+  sidebarComponent: "div.sidebar > div.pitch",
+  markerComponent: "div.markerContainer",
+  homeLinkComponent: "a=Home",
+  filterLinkComponent: "a[href='/filter']",
+  filterListComponent: "div.filter",
+  filterCloseLink: "div.close-filter",
+  filterToggleComponent: "span.react-toggle-label",
+  filterToggleUnchecked: "div.react-toggle:not(.react-toggle--checked)",
+};
+
+export default elements;
--- tests/end-to-end/smokeTest_spec.js
+++ tests/end-to-end/smokeTest_spec.js
@@ -2,62 +2,52 @@
 /* eslint-env jasmine */
 /* eslint-disable func-names, prefer-arrow-callback, no-unused-expressions */
 /* eslint-disable import/no-extraneous-dependencies */
+/* global browser */
+
+import el from "./elements";
 import DEFAULT_FILTER_LIST from "../../imports/state/data/defaultFilters";
 
 describe("Smoke Test", function () {
-  const baseUrl = "http://localhost:3000";
-
-  const locationsMapComponent = "div.map-container";
-  const navbarComponent = "div.nav > div.nav__title";
-  const hamburger = ".toggle-sidebar";
-  const sidebarComponent = "div.sidebar > div.pitch";
-  const markerComponent = "div.markerContainer";
-
   describe("Landing Page", function () {
     it("displays the map and navbar as the main page", function () {
-      browser.url(baseUrl);
+      browser.url(el.baseUrl);
 
       expect(browser.getTitle()).toEqual("Eat Drink Healthy");
-      expect(browser.waitForExist(locationsMapComponent)).toBe(true);
-      expect(browser.waitForExist(navbarComponent)).toBe(true);
+      expect(browser.waitForExist(el.locationsMapComponent)).toBe(true);
+      expect(browser.waitForExist(el.navbarComponent)).toBe(true);
     });
 
     it("displays at least one map marker - NOTE may fail based on location", function () {
-      browser.url(baseUrl);
-      browser.waitForExist(locationsMapComponent);
+      browser.url(el.baseUrl);
+      browser.waitForExist(el.markerComponent);
 
-      const markerArray = browser.elements(markerComponent).value;
+      const markerArray = browser.elements(el.markerComponent).value;
 
       expect(markerArray.length).toBeGreaterThan(1);
     });
   });
 
   describe("Filter Page", function () {
-    const filterLinkComponent = "div=Filter";
-    const filterListComponent = "div.filter";
-    const filterCloseLink = "div.close-filter";
-
     it("displays the filter page when filter link clicked", function () {
-      browser.url(baseUrl);
-      browser.waitForExist(filterLinkComponent);
-      browser.click(filterLinkComponent);
+      browser.url(el.baseUrl);
+      browser.waitForExist(el.filterLinkComponent);
+      browser.click(el.filterLinkComponent);
 
-      expect(browser.waitForExist(filterListComponent)).toBe(true);
+      expect(browser.waitForExist(el.filterListComponent)).toBe(true);
     });
 
     it("has a toggle component for each filter", function () {
-      browser.url(`${baseUrl}/filter`);
-      browser.waitForExist(filterListComponent);
+      browser.url(`${el.baseUrl}/filter`);
+      browser.waitForExist(el.filterListComponent);
 
-      const filterToggleComponent = "span.react-toggle-label";
-      const numberOfFilterToggles = browser.elements(filterToggleComponent).value.length;
+      const numberOfFilterToggles = browser.elements(el.filterToggleComponent).value.length;
 
       expect(numberOfFilterToggles).toBe(DEFAULT_FILTER_LIST.length);
     });
 
     it("has default filters set", function () {
-      browser.url(`${baseUrl}/filter`);
-      browser.waitForExist(filterListComponent);
+      browser.url(`${el.baseUrl}/filter`);
+      browser.waitForExist(el.filterListComponent);
 
       const defaultFiltersOn = DEFAULT_FILTER_LIST.filter(filter => filter.on);
       const togglesOn = "input:checked";
@@ -67,50 +57,47 @@ describe("Smoke Test", function () {
     });
 
     it("returns to the landing page when 'x' is clicked", function () {
-      browser.url(`${baseUrl}/filter`);
-      browser.waitForExist(filterListComponent);
-      browser.click(filterCloseLink);
+      browser.url(`${el.baseUrl}/filter`);
+      browser.waitForExist(el.filterListComponent);
+      browser.click(el.filterCloseLink);
 
-      expect(browser.waitForExist(navbarComponent)).toBe(true);
+      expect(browser.waitForExist(el.navbarComponent)).toBe(true);
     });
 
     it("returns more results when more filters clicked - NOTE may fail based on location", function () {
       // go to the filter page
-      browser.url(`${baseUrl}/filter`);
-      browser.waitForExist(filterListComponent);
+      browser.url(`${el.baseUrl}/filter`);
+      browser.waitForExist(el.filterListComponent);
 
       // get all the unchecked filter toggle elements, and click them
-      const filterToggleUnchecked = "div.react-toggle:not(.react-toggle--checked)";
-      const filterTogglesUnchecked = browser.elements(filterToggleUnchecked).value;
-      filterTogglesUnchecked.forEach(el => el.click());
+      const filterTogglesUnchecked = browser.elements(el.filterToggleUnchecked).value;
+      filterTogglesUnchecked.forEach(elem => elem.click());
 
       // go to the map (landing page)
-      browser.click(filterCloseLink);
-      browser.waitForExist(markerComponent, 3000); // allow for some api response time
+      browser.click(el.filterCloseLink);
+      browser.waitForExist(el.markerComponent, 3000); // allow for some api response time
 
       // check that there are many more results
-      const markerArray = browser.elements(markerComponent).value;
+      const markerArray = browser.elements(el.markerComponent).value;
       expect(markerArray.length).toBeGreaterThan(10);
     });
   });
 
   describe("Sidebar / sign-in Page", function () {
-    const homeLinkComponent = "a=Home";
-
     it("displays the sidebar when hamburger clicked", function () {
-      browser.url(baseUrl);
-      browser.waitForExist(hamburger);
-      browser.click(hamburger);
+      browser.url(el.baseUrl);
+      browser.waitForExist(el.hamburger);
+      browser.click(el.hamburger);
 
-      expect(browser.waitForExist(sidebarComponent)).toBe(true);
+      expect(browser.waitForExist(el.sidebarComponent)).toBe(true);
     });
 
     it("goes back to landing page when home link clicked", function () {
-      browser.url(`${baseUrl}/sidebar`);
-      browser.waitForExist(homeLinkComponent);
-      browser.click(homeLinkComponent);
+      browser.url(`${el.baseUrl}/sidebar`);
+      browser.waitForExist(el.homeLinkComponent);
+      browser.click(el.homeLinkComponent);
 
-      expect(browser.waitForExist(navbarComponent)).toBe(true);
+      expect(browser.waitForExist(el.navbarComponent)).toBe(true);
     });
   });
 });
