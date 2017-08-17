2f98f917bc7230df790e464c5b33d9507c91afd9
stevenjmarsh <sjm.atl@gmail.com>
2017-08-17 11:31:27 -0400

    moving join button to UserMenu component

--- imports/ui/components/Navbar.jsx
+++ imports/ui/components/Navbar.jsx
@@ -10,13 +10,6 @@ const Navbar = (): React$Element<*> => (
       <div className="dtc v-mid w-75 tr">
         <UserMenu />
         <Link
-          id="navbarJoinLink"
-          className="f6 link dim ma1 ph3 pv2 mb2 dib white bg-purple"
-          to="/sidebar"
-        >
-          Join
-        </Link>
-        <Link
           id="navbarFilterLink"
           className="f6 link dim ma1 ph3 pv2 mb2 dib white bg-dark-green"
           to="/filter"
--- imports/ui/components/UserMenu.jsx
+++ imports/ui/components/UserMenu.jsx
@@ -1,8 +1,18 @@
 // @flow
 import React from "react";
+import { Link } from "react-router-dom";
 
 const UserMenu = (): React$Element<*> => (
-  <span>UserMenu</span>
+  <span>
+    <span>UserMenu</span>
+    <Link
+      id="navbarJoinLink"
+      className="f6 link dim ma1 ph3 pv2 mb2 dib white bg-purple"
+      to="/sidebar"
+    >
+      Join
+    </Link>
+  </span>
 );
 
 export default UserMenu;
