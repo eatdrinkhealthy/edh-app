bfa493569d377dca5ee8a0efde1744b717cf8dda
stevenjmarsh <sjm.atl@gmail.com>
2017-08-17 11:53:43 -0400

    converting UserMenu from functional to class component

--- imports/ui/components/UserMenu.jsx
+++ imports/ui/components/UserMenu.jsx
@@ -1,18 +1,22 @@
 // @flow
-import React from "react";
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
-const UserMenu = (): React$Element<*> => (
-  <span>
-    <span>UserMenu</span>
-    <Link
-      id="navbarJoinLink"
-      className="f6 link dim ma1 ph3 pv2 mb2 dib white bg-purple"
-      to="/sidebar"
-    >
-      Join
-    </Link>
-  </span>
-);
+class UserMenu extends Component {
+  render() {
+    return (
+      <span>
+        <span>UserMenu</span>
+        <Link
+          id="navbarJoinLink"
+          className="f6 link dim ma1 ph3 pv2 mb2 dib white bg-purple"
+          to="/sidebar"
+        >
+        Join
+        </Link>
+      </span>
+    );
+  }
+}
 
 export default UserMenu;
