3fd7ab42ec4e615daa6e63ff1b113ee6f10d4b21
stevenjmarsh <sjm.atl@gmail.com>
2017-08-17 11:43:20 -0400

    creating jest snapshot for UserMenu

---
+++ imports/ui/components/tests/UserMenu.jest.jsx
@@ -0,0 +1,19 @@
+// @flow
+/* eslint-env jest */
+/* eslint-disable func-names, prefer-arrow-callback, no-unused-expressions */
+/* eslint-disable import/no-extraneous-dependencies */
+import React from "react";
+import { MemoryRouter } from "react-router-dom";
+import renderer from "react-test-renderer";
+import UserMenu from "../UserMenu";
+
+describe("<UserMenu", function () {
+  it("matches render snapshot", function () {
+    const tree = renderer.create(
+      <MemoryRouter>
+        <UserMenu />
+      </MemoryRouter>,
+    ).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
