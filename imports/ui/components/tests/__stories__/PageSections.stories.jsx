4cc2e7726921e7a2231e0f4aed2eba2a8c021e7a
stevenjmarsh <sjm.atl@gmail.com>
2017-08-24 12:46:28 -0400

    removing Navbar from storybook (contains a container)

--- imports/ui/components/tests/__stories__/PageSections.stories.jsx
+++ imports/ui/components/tests/__stories__/PageSections.stories.jsx
@@ -6,16 +6,12 @@ import { MemoryRouter } from "react-router-dom";
 import { storiesOf, action } from "@storybook/react";
 import { text } from "@storybook/addon-knobs";
 
-import Navbar from "../../Navbar";
 import CreateAccount from "../../CreateAccount";
 import UserMenu from "../../UserMenu";
 
 storiesOf("Page Sections", module)
   .addDecorator((story: () => React$Element<*>): React$Element<*> => (
     <MemoryRouter>{story()}</MemoryRouter>
-  ))
-  .add("Navbar", (): React$Element<*> => (
-    <Navbar />
   ))
   .add("CreateAccount", (): React$Element<*> => (
     <CreateAccount handleSubmit={action("submitted")} />
