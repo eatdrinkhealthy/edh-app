6708658589e4f61698e16c8d9cbfb27b2b2fca34
stevenjmarsh <sjm.atl@gmail.com>
2017-09-01 17:06:14 -0400

    adding initial form error message fields for join (driven by state)

--- imports/ui/components/CreateAccount.jsx
+++ imports/ui/components/CreateAccount.jsx
@@ -1,5 +1,6 @@
 // @flow
 import React, { Component } from "react";
+import classNames from "classnames";
 import AlertMessage from "./AlertMessage";
 
 class CreateAccount extends Component {
@@ -12,6 +13,12 @@ class CreateAccount extends Component {
     email: "",
     password: "",
     confirmPassword: "",
+    formErrors: {
+      username: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+    },
   };
 
   componentDidMount() {
@@ -19,6 +26,24 @@ class CreateAccount extends Component {
   }
 
   usernameInput: HTMLInputElement;
+
+  renderInputError = (errorMessage: string) => (
+    <div className="ml2 mt1 dark-red">{errorMessage}</div>
+  );
+
+  labelClassName = (error: boolean): string => (
+    classNames(
+      "db fw6 lh-copy f6",
+      { "dark-red": error },
+    )
+  );
+
+  inputClassName = (error: boolean): string => (
+    classNames(
+      "pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100",
+      { "dark-red": error },
+    )
+  );
 
   resetForm = () => {
     this.setState({
@@ -28,7 +53,7 @@ class CreateAccount extends Component {
       confirmPassword: "",
     });
     this.usernameInput.focus();
-  }
+  };
 
   handleInputChange = (event: Event) => {
     // NOTE: although you see this handleInputChange example in react docs, it is
@@ -41,7 +66,7 @@ class CreateAccount extends Component {
     this.setState({
       [name]: value,
     });
-  }
+  };
 
   onSubmit = (event: Event) => {
     event.preventDefault();
@@ -58,7 +83,7 @@ class CreateAccount extends Component {
       this.resetForm();
       this.props.handleSubmit(this.state.username, this.state.email, this.state.password);
     }
-  }
+  };
 
   render() {  // eslint-disable-line flowtype/require-return-type
     return (
@@ -70,9 +95,14 @@ class CreateAccount extends Component {
           <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
             <legend className="f4 fw6 ph0 mh0">Create Account</legend>
             <div className="mt3">
-              <label className="db fw6 lh-copy f6" htmlFor="username">Username</label>
+              <label
+                className={this.labelClassName(!!this.state.formErrors.username)}
+                htmlFor="username"
+              >
+                Username
+              </label>
               <input
-                className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                className={this.inputClassName(!!this.state.formErrors.username)}
                 type="text"
                 name="username"
                 id="username"
@@ -82,39 +112,58 @@ class CreateAccount extends Component {
                   this.usernameInput = el;
                 }}
               />
+              {this.renderInputError(this.state.formErrors.username)}
             </div>
             <div className="mt3">
-              <label className="db fw6 lh-copy f6" htmlFor="email">Email</label>
+              <label
+                className={this.labelClassName(!!this.state.formErrors.email)}
+                htmlFor="email"
+              >
+                Email
+              </label>
               <input
-                className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                className={this.inputClassName(!!this.state.formErrors.email)}
                 type="email"
                 name="email"
                 id="email"
                 value={this.state.email}
                 onChange={this.handleInputChange}
               />
+              {this.renderInputError(this.state.formErrors.email)}
             </div>
             <div className="mv3">
-              <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
+              <label
+                className={this.labelClassName(!!this.state.formErrors.password)}
+                htmlFor="password"
+              >
+                Password
+              </label>
               <input
-                className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                className={this.inputClassName(!!this.state.formErrors.password)}
                 type="password"
                 name="password"
                 id="password"
                 value={this.state.password}
                 onChange={this.handleInputChange}
               />
+              {this.renderInputError(this.state.formErrors.password)}
             </div>
             <div className="mv3">
-              <label className="db fw6 lh-copy f6" htmlFor="confirmPassword">Confirm Password</label>
+              <label
+                className={this.labelClassName(!!this.state.formErrors.confirmPassword)}
+                htmlFor="confirmPassword"
+              >
+                Confirm Password
+              </label>
               <input
-                className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                className={this.inputClassName(!!this.state.formErrors.confirmPassword)}
                 type="password"
                 name="confirmPassword"
                 id="confirmPassword"
                 value={this.state.confirmPassword}
                 onChange={this.handleInputChange}
               />
+              {this.renderInputError(this.state.formErrors.confirmPassword)}
             </div>
           </fieldset>
           <div>
