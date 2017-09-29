d2d30cdd4f85ffec8792bfb85c24bb67d8134356
stevenjmarsh <sjm.atl@gmail.com>
2017-09-29 13:39:43 -0400

    using ValidationError as a meteor module instead of a global (flow type)

--- .types/mdg-validation-error.js
+++ .types/mdg-validation-error.js
@@ -1,18 +1,21 @@
-declare type IValidationErrorDetail = {
-  name: string,
-  value: string,
-  type: string,
-  dataType: string,
-  message: string,
-};
+declare module "meteor/mdg:validation-error" {
+  declare export type IValidationErrorDetail = {
+    name: string,
+    type: string,
+    message?: string,
+    value?: string | boolean | number,
+    dataType?: string,
+    min?: number,
+    max?: number,
+  };
 
-declare class IValidationError {
-  constructor(error: Array<IValidationErrorDetail>, message?: string): IValidationError;
+  declare export class IValidationError {
+    constructor(errors: Array<IValidationErrorDetail>, message?: string): IValidationError;
 
-  errorType: string | number;
-  name: string;
-  error: string;
-  details: Array<IValidationErrorDetail>;
-  is?: (any) => boolean;
-};
-
+    errorType: string | number;
+    name: string;
+    error: string;
+    details: Array<IValidationErrorDetail>;
+    is?: (any) => boolean;
+  }
+}
