dc193868b7f425e7e58e518ff0b0cd5f689bea6f
stevenjmarsh <sjm.atl@gmail.com>
2017-10-11 17:54:08 -0400

    adding react-bootstrap package (and a sample button for now)

--- imports/ui/pages/HomePage.jsx
+++ imports/ui/pages/HomePage.jsx
@@ -1,5 +1,6 @@
 // @flow
 import React from "react";
+import Button from "react-bootstrap/lib/Button";
 import { Link } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import MapContainer from "../containers/MapContainer";
@@ -17,6 +18,7 @@ const HomePage = () => (
       >
         Filter
       </Link>
+      <Button href="/filter" >Filter (button component)</Button>
     </div>
   </div>
 );
