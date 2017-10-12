bf8c0744efa55244d7447f3e14c3fe2f8b1fbc07
stevenjmarsh <sjm.atl@gmail.com>
2017-10-12 17:11:40 -0400

    adding new logo to navbar

--- imports/ui/components/Navbar.jsx
+++ imports/ui/components/Navbar.jsx
@@ -7,10 +7,11 @@ const Navbar = (): React$Element<*> => (
   <div id="navbar" className="nav container-fluid">
     <Grid>
       <Row>
-        <Col xs={6}>
+        <Col xs={4} offset={4}>
+          <img className="nav__logo" src="/images/EDH-logo.png" alt="Eat Drink Healthy logo" />
           <div className="nav__title">Eat Drink Healthy</div>
         </Col>
-        <Col xs={6}>
+        <Col xs={4}>
           <UserMenuContainer />
         </Col>
       </Row>
