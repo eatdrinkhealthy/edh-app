745248431246633182dbd18a0de492a9b70551fc
stevenjmarsh <sjm.atl@gmail.com>
2017-10-13 15:57:37 -0400

    styling navbar: vertical centering items, adding Blanch font

--- imports/ui/components/Navbar.jsx
+++ imports/ui/components/Navbar.jsx
@@ -1,21 +1,16 @@
 // @flow
 import React from "react";
-import { Grid, Row, Col } from "../helpers/reactBootstrapLib";
 import UserMenuContainer from "../containers/UserMenuContainer";
 
 const Navbar = (): React$Element<*> => (
-  <div id="navbar" className="nav container-fluid">
-    <Grid>
-      <Row>
-        <Col xs={4} offset={4}>
-          <img className="nav__logo" src="/images/EDH-logo.png" alt="Eat Drink Healthy logo" />
-          <div className="nav__title">Eat Drink Healthy</div>
-        </Col>
-        <Col xs={4}>
-          <UserMenuContainer />
-        </Col>
-      </Row>
-    </Grid>
+  <div id="navbar" className="nav">
+    <img
+      className="nav__logo verticalCenterFudge"
+      src="/images/EDH-logo.png"
+      alt="Eat Drink Healthy logo"
+    />
+    <span className="nav__title">EAT.&nbsp;DRINK.&nbsp;HEALTHY.</span>
+    <span className="nav__usermenu"><UserMenuContainer /></span>
   </div>
 );
 
