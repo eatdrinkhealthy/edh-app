51e47b1b04cb3015226b53588d0e84d37f67e792
stevenjmarsh <sjm.atl@gmail.com>
2017-10-16 17:22:24 -0400

    improving navbar responsiveness, vertical centering, updating username label (welcome)

--- imports/ui/components/tests/UserMenu.jest.jsx
+++ imports/ui/components/tests/UserMenu.jest.jsx
@@ -30,7 +30,7 @@ describe("<UserMenu />", function () {
 
   it("should display the username when passed a user name", function () {
     const wrapper = shallow(<UserMenu username="testUser" logout={jest.fn()} />);
-    expect(wrapper.find(es.userMenu.username).text()).toBe("Username: testUser");
+    expect(wrapper.find(es.userMenu.username).text()).toBe("Welcome, testUser!");
   });
 
   it("should not show login & join buttons when logged in, and show logout", function () {
