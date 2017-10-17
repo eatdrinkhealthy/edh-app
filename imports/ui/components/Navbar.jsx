9ae465990a6dee3c0a6d4afca4157303a868d6ec
stevenjmarsh <sjm.atl@gmail.com>
2017-10-17 11:10:27 -0400

    adding grid back to navbar, simpler

--- imports/ui/components/Navbar.jsx
+++ imports/ui/components/Navbar.jsx
@@ -1,16 +1,25 @@
 // @flow
 import React from "react";
+import { Grid, Row, Col } from "../helpers/reactBootstrapLib";
 import UserMenuContainer from "../containers/UserMenuContainer";
 
 const Navbar = (): React$Element<*> => (
   <div id="navbar" className="nav__bar">
-    <img
-      className="nav__logo"
-      src="/images/EDH-logo.png"
-      alt="Eat Drink Healthy logo"
-    />
-    <span className="nav__title">EAT. DRINK. HEALTHY.</span>
-    <span className="nav__usermenu"><UserMenuContainer /></span>
+    <Grid fluid>
+      <Row>
+        <Col xs={6} className="show-grid">
+          <img
+            className="nav__logo"
+            src="/images/EDH-logo.png"
+            alt="Eat Drink Healthy logo"
+          />
+          <span className="nav__title">EAT. DRINK. HEALTHY.</span>
+        </Col>
+        <Col xs={6} className="show-grid">
+          <span className="nav__usermenu"><UserMenuContainer /></span>
+        </Col>
+      </Row>
+    </Grid>
   </div>
 );
 
