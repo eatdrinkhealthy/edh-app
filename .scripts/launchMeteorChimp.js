0f8f1d59c292bfeca133ab3db0460a7e10483b4c
stevenjmarsh <sjm.atl@gmail.com>
2017-10-19 10:48:16 -0400

    adding a node script to launch meteor test server & chimp end to end tests

---
+++ .scripts/launchMeteorChimp.js
@@ -0,0 +1,27 @@
+// Referenced from: https://github.com/xolvio/chimp/issues/485
+// Inspired from: https://github.com/xolvio/qualityfaster/blob/master/.scripts/
+const { exec } = require("child_process");
+
+const meteorCommand = process.argv[2];
+const chimpCommand = process.argv[3];
+
+const meteorProcess = exec(meteorCommand); // 'npm start'
+
+meteorProcess.stdout.pipe(process.stdout);
+meteorProcess.stderr.pipe(process.stderr);
+
+// Wait until Meteor is started and then start the chimp tests
+meteorProcess.stdout.on('data', function (data) {
+  if (data.toString().match('App running at')) {
+    const chimpProcess = exec(chimpCommand, { maxBuffer: 20480000 }); // 'npm run test:chimp:phantomjs'
+
+    chimpProcess.stdout.pipe(process.stdout);
+    chimpProcess.stderr.pipe(process.stderr);
+
+    chimpProcess.on('close', (code) => {
+      console.log(`Chimp exited with code ${code}`);
+      meteorProcess.kill();
+      process.exit(code);
+    });
+  }
+});
