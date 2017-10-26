a9f5d436a42c4267f94a947ce059a1ce3d229407
stevenjmarsh <sjm.atl@gmail.com>
2017-10-26 13:05:48 -0400

    creating an HOC to display border and breakpoint for Col & Grid

--- imports/ui/components/ReactBootstrapLib.jsx
+++ imports/ui/components/ReactBootstrapLib.jsx
@@ -1,6 +1,6 @@
 // NO Flow (primarily to avoid making libdefs for each react-bootstrap component lib)
 /* eslint-disable react/require-default-props */
-import React from "react";
+import React, { Component } from "react";
 import PropTypes from "prop-types";
 import classNames from "classnames";
 import GridBase from "react-bootstrap/lib/Grid";
@@ -15,40 +15,34 @@ export { Button };
 export { ToggleButton };
 export { ToggleButtonGroup };
 
-export const Col = (props) => {
-  const { className, children, ...otherProps } = props;
-  const allClasses = classNames(className, { "show-grid": window.SHOW_GRID });
+// This HOC takes a component, and will add the additional passed in class
+// to className, if the global variable SHOW_GRID is set.
+const showGridHOC = (
+  WrappedComponent,
+  gridClass,
+) => {
+  const GridHOC = (props) => {
+    const { className, ...otherProps } = props;
+    const allClasses = classNames(className, { [gridClass]: window.SHOW_GRID });
 
-  return (
-    <ColBase
-      {...otherProps}
-      className={allClasses}
-    >
-      {children}
-    </ColBase>
-  );
+    return (
+      <WrappedComponent
+        {...otherProps}
+        className={allClasses}
+      />
+    );
+  };
+
+  GridHOC.propTypes = {
+    className: PropTypes.string,
+  };
+
+  GridHOC.displayName =
+    `HOC(${WrappedComponent.displayName || WrappedComponent.name || "Component"})`;
+
+  return GridHOC;
 };
 
-Col.propTypes = {
-  className: PropTypes.string,
-  children: PropTypes.node,
-};
+export const Col = showGridHOC(ColBase, "show-grid");
 
-export const Grid = (props) => {
-  const { className, children, ...otherProps } = props;
-  const allClasses = classNames(className, { "show-breakpoint": window.SHOW_GRID });
-
-  return (
-    <GridBase
-      {...otherProps}
-      className={allClasses}
-    >
-      {children}
-    </GridBase>
-  );
-};
-
-Grid.propTypes = {
-  className: PropTypes.string,
-  children: PropTypes.node,
-};
+export const Grid = showGridHOC(GridBase, "show-breakpoint");
--- imports/ui/components/tests/ReactBootstrapLib.jest.jsx
+++ imports/ui/components/tests/ReactBootstrapLib.jest.jsx
@@ -4,10 +4,16 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import React from "react";
 import { mount } from "enzyme";
-import { Col } from "../ReactBootstrapLib";
+import { Grid, Col } from "../ReactBootstrapLib";
 
 describe("ReactBootstrapLib", function () {
   describe("Col - with global show-grid setting", function () {
+    it("should have a composed HOC displayName", function () {
+      window.SHOW_GRID = true;
+      const wrapper = mount(<Col>test text</Col>);
+      expect(wrapper.name()).toBe("HOC(Col)");
+    });
+
     it("should add show-grid class when window.SHOW_GRID is true", function () {
       window.SHOW_GRID = true;
       const wrapper = mount(<Col><span>test text</span></Col>);
@@ -40,6 +46,26 @@ describe("ReactBootstrapLib", function () {
       expect(wrapper.find("div").prop("className")).toContain("thisClass");
       expect(wrapper.find("div").prop("className")).toContain("col-xs-4");
       expect(wrapper.find("div").prop("className")).toContain("show-grid");
+    });
+  });
+
+  describe("Grid - with global show-grid setting", function () {
+    it("should have a composed HOC displayName", function () {
+      window.SHOW_GRID = true;
+      const wrapper = mount(<Grid>test text</Grid>);
+      expect(wrapper.name()).toBe("HOC(Grid)");
+    });
+
+    it("should add show-breakpoint class when window.SHOW_GRID is true", function () {
+      window.SHOW_GRID = true;
+      const wrapper = mount(<Grid><span>test text</span></Grid>);
+      expect(wrapper.find("div").prop("className")).toContain("show-breakpoint");
+    });
+
+    it("should NOT add show-breakpoint class when window.SHOW_GRID is false", function () {
+      window.SHOW_GRID = false;
+      const wrapper = mount(<Grid><span>test text</span></Grid>);
+      expect(wrapper.find("div").prop("className")).not.toContain("show-breakpoint");
     });
   });
 });
