7cb3740569aaf2effdd6f7a4f7accb102ed7d63e
stevenjmarsh <sjm.atl@gmail.com>
2017-10-30 10:50:44 -0400

    adding initial tests for FilterSection

---
+++ imports/ui/components/tests/FilterSection.jest.jsx
@@ -0,0 +1,60 @@
+// @flow
+/* eslint-env jest */
+/* eslint-disable func-names, prefer-arrow-callback, no-unused-expressions */
+/* eslint-disable import/no-extraneous-dependencies */
+import React from "react";
+import renderer from "react-test-renderer";
+import FilterSection from "../FilterSection";
+import type { IFilter } from "../../../state/reducers/filtersReducers";
+
+describe("<FilterSection />", function () {
+  const testFilterList: Array<IFilter> = [
+    {
+      id: "juiceBar1",
+      name: "Juice Bar1",
+      on: true,
+      foursquareCategory: "1",
+    },
+    {
+      id: "juiceBar2",
+      name: "Juice Bar2",
+      on: false,
+      foursquareCategory: "2",
+    },
+    {
+      id: "juiceBar3",
+      name: "Juice Bar3",
+      on: false,
+      foursquareCategory: "3",
+    },
+    {
+      id: "juiceBar4",
+      name: "Juice Bar4",
+      on: true,
+      foursquareCategory: "4",
+    },
+    {
+      id: "juiceBar5",
+      name: "Juice Bar5",
+      on: false,
+      foursquareCategory: "5",
+    },
+    {
+      id: "juiceBar6",
+      name: "Juice Bar6",
+      on: false,
+      foursquareCategory: "6",
+    },
+  ];
+
+  it("matches render snapshot", function () {
+    const tree = renderer.create(
+      <FilterSection
+        title="Test Filter Section"
+        filters={testFilterList}
+      />,
+    ).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+});
