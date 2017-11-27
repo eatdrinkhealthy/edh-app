df2aa5e0d63534aaf5465877a6644e31ffc3aff5
stevenjmarsh <sjm.atl@gmail.com>
2017-11-27 12:21:05 -0500

    adding flow Geolocation function definitions

--- .types/mdg-geolocation.js
+++ .types/mdg-geolocation.js
@@ -1,1 +1,9 @@
-//
+// NOTE: the mdg:geolocation package exposes Geolocation on the client
+declare var Geolocation: {
+  error(): ?PositionError,
+  currentLocation(): ?Position,
+  latLng(): ?{
+    lat: number,
+    lng: number,
+  }
+}
