7255092d413f2b163da9f04fcd3668e372b0baeb
stevenjmarsh <sjm.atl@gmail.com>
2017-12-12 15:37:13 -0500

    making 'selectedColor' a prop for Pill

--- imports/ui/components/Pill.jsx
+++ imports/ui/components/Pill.jsx
@@ -6,15 +6,27 @@ type IPillProps = {
   className?: string,   // eslint-disable-line react/require-default-props
   name: string,
   active: boolean,
+  selectedColor?: string,
   onClick: (*) => void,
 };
 
 const Pill = ({
   className: excludeClassName,  // extract out and override className
   active,                       // use active, but don't pass it on to button
+  selectedColor,
   ...props                      // pass on the rest of the props (used by a button)
-}: IPillProps) => (
-  <button className={classNames("pill", { pill_selected: active })} {...props} />
-);
+}: IPillProps) => {
+  // TODO find better way to optimize styling (all here, or in .less file)
+  const colorStyle = {
+    background: active ? selectedColor : "white",
+    borderColor: active ? selectedColor : "#ccc",
+  };
+
+  return <button style={colorStyle} className={classNames("pill", { pill_selected: active })} {...props} />;
+};
+
+Pill.defaultProps = {
+  selectedColor: "#5e2ca5",
+};
 
 export default Pill;
